0fb0b6b5e6b34f6d27fa5489ebe5129b9614a12c
Jon Bauman <jongithub@shumi.fake>
2015-11-05 12:31:49 -0800

    make 'convert' pass 'breakfast'

--- warm-ups/building-hashes.rb
+++ warm-ups/building-hashes.rb
@@ -2,12 +2,16 @@ require "rspec"
 
 describe :breakfast do
   input = "first_name=Ada&last_name=Lovelace&dob=1815&title=Countess"
-  output = {first_name: "Ada", last_name: "Lovelace", title: "Countess"}
+  output = {first_name: "Ada", last_name: "Lovelace", dob: "1815", title: "Countess"}
   it "convert returns #{output} for #{input}" do
     expect(convert(input)).to eq(output)
   end
 end
 
 def convert(input)
-  nil
+  key_value_pairs = input.split("&").map do |key_value_string|
+    key, value = key_value_string.split("=")
+    [key.to_sym, value]
+  end
+  Hash[key_value_pairs]
 end
